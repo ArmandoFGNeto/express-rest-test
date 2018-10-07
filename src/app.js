e5899bef254a711d723e951fa1ed8c1d036a76e1
Armando <armandofgneto@gmail.com>
2018-10-07 19:29:20 -0300

    Add base url
    Added base url rest/v1 for products GET
    endpoint

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ var products = require("./routes/products.route.js");
 
 var app = express();
 mongoose.connect('mongodb://localhost:27017/products', {useNewUrlParser: true});
-app.use('/products', products)
+
+app.use('/rest/v1/products', products)
 
 var server = app.listen(8080, function () {
     console.log("App running on port.", server.address().port);
