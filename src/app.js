21d01903d39f294a76923472aa142994cb21daab
Armando <armandofgneto@gmail.com>
2018-10-09 22:17:43 -0300

    Add CORS support
    Added CORS support to the app

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 var express = require("express");
 var mongoose = require('mongoose');
+var cors = require('cors');
 
 var products = require("./routes/products.route.js");
 
 var app = express();
 mongoose.connect('mongodb://localhost:27017/products', {useNewUrlParser: true});
 
+app.use(cors());
 app.use('/rest/v1/products', products)
 
 var server = app.listen(8080, function () {
